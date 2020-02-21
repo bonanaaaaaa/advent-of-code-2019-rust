1e818fcb2f439f222e14a0bbc40f134b73008cb2
Pittawat Mongkonrat <pittawatm@gmail.com>
2020-02-21 20:38:40 +0700

    :bug: :recycle: Day 4 Part 1

--- src/day04/part1.rs
+++ src/day04/part1.rs
@@ -1,37 +1,21 @@
-use crate::reader;
+const START: i32 = 271973;
+const END: i32 = 785961;
 
 pub fn run() {
   println!("Day 4 Part 1");
-  let contents = reader::read("src/day04/input1.txt".to_string());
-
-  let num: Vec<i32> = contents.split("-").map(|s| s.parse().unwrap()).collect();
-  let start = num[0];
-  let end = num[1];
 
   let mut count = 0;
 
   for i in 1..10 {
-    count += think(start, end, false, false, i, 4, i * 10i32.pow(5))
+    count += think(false, i, 4, i * 10i32.pow(5));
   }
 
   println!("{}", count);
 }
 
-fn think(
-  start: i32,
-  end: i32,
-  repeat: bool,
-  is_decrease: bool,
-  num: i32,
-  digit: i32,
-  digits: i32,
-) -> i32 {
-  if is_decrease {
-    return 0;
-  }
+fn think(repeat: bool, num: i32, digit: i32, digits: i32) -> i32 {
   if digit < 0 {
-    if repeat && !is_decrease && start <= digits && digits <= end {
-      // println!("{}", digits);
+    if repeat && START <= digits && digits <= END {
       return 1;
     } else {
       return 0;
@@ -40,12 +24,9 @@ fn think(
 
   let mut count = 0;
 
-  for i in 0..10 {
+  for i in num..10 {
     count += think(
-      start,
-      end,
-      repeat && num == i,
-      is_decrease || i < num,
+      repeat || num == i,
       i,
       digit - 1,
       digits + (i * 10i32.pow(digit as u32)),
