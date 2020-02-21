b1a953c40172eaa8f97ed14931a3021c16fc14c1
Pittawat Mongkonrat <pittawatm@gmail.com>
2020-02-21 20:38:50 +0700

    :recycle: Day 4 part 2

--- src/day04/part2.rs
+++ src/day04/part2.rs
@@ -1,74 +1,131 @@
-use crate::reader;
+#[derive(Copy, Clone, Debug)]
+enum State {
+  NoPair,
+  Pair2,
+  PairMoreThan2,
+  Chill,
+  Accept,
+  Reject,
+}
+
+const START: i32 = 271973;
+const END: i32 = 785961;
 
 pub fn run() {
   println!("Day 4 Part 2");
-  let contents = reader::read("src/day04/input2.txt".to_string());
-
-  let num: Vec<i32> = contents.split("-").map(|s| s.parse().unwrap()).collect();
-  let start = num[0];
-  let end = num[1];
 
   let mut count = 0;
 
   for i in 1..10 {
-    count += think(start, end, 0, Vec::new(), i, 4, i * 10i32.pow(5))
+    count += compute(State::NoPair, i, 4, i * 10i32.pow(5));
   }
 
   println!("{}", count);
 }
 
-fn think(
-  start: i32,
-  end: i32,
-  repeat: i32,
-  his: Vec<i32>,
-  num: i32,
-  digit: i32,
-  digits: i32,
-) -> i32 {
-  if digit < 0 {
-    if check(his, repeat) && start <= digits && digits <= end {
-      return 1;
-    } else {
-      return 0;
+fn compute(state: State, number: i32, digit: i32, value: i32) -> i32 {
+  match state {
+    State::NoPair => {
+      let mut count = 0;
+      count += compute(
+        if digit - 1 < 0 {
+          State::Accept
+        } else {
+          State::Pair2
+        },
+        number,
+        digit - 1,
+        value + (number * 10i32.pow(digit as u32)),
+      );
+      for i in (number + 1)..10 {
+        count += compute(
+          if digit - 1 < 0 {
+            State::Reject
+          } else {
+            State::NoPair
+          },
+          i,
+          digit - 1,
+          value + (i * 10i32.pow(digit as u32)),
+        );
+      }
+      count
     }
-  }
-
-  let mut count = 0;
-
-  for i in num..10 {
-    let r = if num == i { repeat + 1 } else { 0 };
-
-    let h = if num != i {
-      let mut n_vec = his.clone();
-      n_vec.push(repeat);
-      n_vec.to_vec()
-    } else {
-      his.clone()
-    };
-
-    count += think(
-      start,
-      end,
-      r,
-      h,
-      i,
-      digit - 1,
-      digits + (i * 10i32.pow(digit as u32)),
-    );
-  }
-
-  count
-}
-
-fn check(v: Vec<i32>, r: i32) -> bool {
-  if r == 1 {
-    return true;
-  }
-  for n in v {
-    if n == 1 {
-      return true;
+    State::Pair2 => {
+      let mut count = 0;
+      count += compute(
+        if digit - 1 < 0 {
+          State::Reject
+        } else {
+          State::PairMoreThan2
+        },
+        number,
+        digit - 1,
+        value + (number * 10i32.pow(digit as u32)),
+      );
+      for i in number + 1..10 {
+        count += compute(
+          if digit - 1 < 0 {
+            State::Accept
+          } else {
+            State::Chill
+          },
+          i,
+          digit - 1,
+          value + (i * 10i32.pow(digit as u32)),
+        );
+      }
+      count
     }
+    State::PairMoreThan2 => {
+      let mut count = 0;
+      count += compute(
+        if digit - 1 < 0 {
+          State::Reject
+        } else {
+          State::PairMoreThan2
+        },
+        number,
+        digit - 1,
+        value + (number * 10i32.pow(digit as u32)),
+      );
+      for i in number + 1..10 {
+        count += compute(
+          if digit - 1 < 0 {
+            State::Reject
+          } else {
+            State::NoPair
+          },
+          i,
+          digit - 1,
+          value + (i * 10i32.pow(digit as u32)),
+        );
+      }
+      count
+    }
+    State::Chill => {
+      let mut count = 0;
+      for i in number..10 {
+        count += compute(
+          if digit - 1 < 0 {
+            State::Accept
+          } else {
+            State::Chill
+          },
+          i,
+          digit - 1,
+          value + (i * 10i32.pow(digit as u32)),
+        );
+      }
+      count
+    }
+    State::Accept => {
+      if START <= value && value <= END {
+        1
+      } else {
+        0
+      }
+    }
+    State::Reject => 0,
   }
-  false
 }
