cd883754798bdb3407e74b5d3eb02798e3d5c413
Pittawat Mongkonrat <pittawatm@gmail.com>
2020-02-25 01:03:11 +0700

    :zap: Day 7

--- src/day07/part2.rs
+++ src/day07/part2.rs
@@ -1,5 +1,8 @@
 use crate::reader;
 
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
+
 pub fn run() {
   println!("Day 7 Part 2");
 
@@ -10,40 +13,91 @@ pub fn run() {
     .map(|s| s.trim().parse().unwrap())
     .collect();
 
-  let all_possible = generate_all_possible_phase_setting();
+  let sequences = generate_all_possible_phase_setting();
+  let sequences_len = sequences.len();
 
   let mut max_output = std::i32::MIN;
 
-  for sequence in all_possible {
-    let mut input = 0;
-    let mut computers: Vec<IntCodeComputer> = sequence
-      .iter()
-      .map(|&n| IntCodeComputer::new(&op_code, n))
-      .collect();
+  let (tx, rx) = channel();
 
-    let mut counter = 0;
-    let computer_len = computers.len();
-    let mut last_value = 0;
+  let mut children = Vec::new();
 
-    loop {
-      let computer = &mut computers[counter as usize];
-      match computer.compute(input) {
-        Ok(output) => {
-          input = output;
-        }
-        Err(_) => {
-          break;
-        }
+  for sequence in sequences {
+    let op_c = op_code.clone();
+    let tx = tx.clone();
+    let child = thread::spawn(move || {
+      let (tx1, rx1) = channel();
+      let (tx2, rx2) = channel();
+      let (tx3, rx3) = channel();
+      let (tx4, rx4) = channel();
+      let (tx5, rx5) = channel();
+      let mut channel_list: Vec<(Sender<i32>, Receiver<i32>)> = Vec::new();
+      for _ in 0..sequences_len {
+        channel_list.push(channel());
       }
 
-      counter = if counter + 1 >= computer_len {
-        last_value = input;
-        0
-      } else {
-        counter + 1
-      };
-    }
-    max_output = max_output.max(last_value);
+      tx5.send(sequence[0]).unwrap();
+      tx5.send(0).unwrap();
+      tx1.send(sequence[1]).unwrap();
+      tx2.send(sequence[2]).unwrap();
+      tx3.send(sequence[3]).unwrap();
+      tx4.send(sequence[4]).unwrap();
+
+      let mut children = Vec::new();
+
+      let op_c1 = op_c.clone();
+      let c1 = thread::spawn(move || {
+        let mut computer1 = IntCodeComputer::new(&op_c1, &tx1, &rx5);
+        computer1.compute().unwrap();
+      });
+      let op_c2 = op_c.clone();
+      let c2 = thread::spawn(move || {
+        let mut computer2 = IntCodeComputer::new(&op_c2, &tx2, &rx1);
+        computer2.compute().unwrap();
+      });
+      let op_c3 = op_c.clone();
+      let c3 = thread::spawn(move || {
+        let mut computer3 = IntCodeComputer::new(&op_c3, &tx3, &rx2);
+        computer3.compute().unwrap();
+      });
+      let op_c4 = op_c.clone();
+      let c4 = thread::spawn(move || {
+        let mut computer4 = IntCodeComputer::new(&op_c4, &tx4, &rx3);
+        computer4.compute().unwrap();
+      });
+      let op_c5 = op_c.clone();
+      let c5 = thread::spawn(move || {
+        let mut computer5 = IntCodeComputer::new(&op_c5, &tx5, &rx4);
+        match computer5.compute() {
+          Ok(result) => {
+            tx.send(result).unwrap();
+          }
+          Err(e) => {
+            eprintln!("Error: {}", e);
+          }
+        }
+      });
+
+      children.push(c1);
+      children.push(c2);
+      children.push(c3);
+      children.push(c4);
+      children.push(c5);
+
+      for c in children {
+        c.join().unwrap();
+      }
+    });
+    children.push(child);
+  }
+
+  for _ in 0..sequences_len {
+    let output = rx.recv().unwrap();
+    max_output = max_output.max(output);
+  }
+
+  for child in children {
+    child.join().unwrap();
   }
   println!("{}", max_output);
 }
@@ -78,24 +132,30 @@ fn run_pick(data: Vec<i32>, result: Vec<i32>, ans: &mut Vec<Vec<i32>>) {
   }
 }
 
-struct IntCodeComputer {
+struct IntCodeComputer<'a> {
   op_code: Vec<i32>,
-  phase_setting: i32,
+  sender: &'a Sender<i32>,
+  receiver: &'a Receiver<i32>,
   ptr: usize,
-  is_phase_setting_used: bool,
+  current_output: i32,
 }
 
-impl IntCodeComputer {
-  fn new(op_code: &Vec<i32>, phase_setting: i32) -> Self {
+impl IntCodeComputer<'_> {
+  fn new<'a>(
+    op_code: &Vec<i32>,
+    sender: &'a Sender<i32>,
+    receiver: &'a Receiver<i32>,
+  ) -> IntCodeComputer<'a> {
     IntCodeComputer {
       op_code: op_code.clone(),
-      phase_setting: phase_setting,
+      sender: sender,
+      receiver: receiver,
       ptr: 0,
-      is_phase_setting_used: false,
+      current_output: 0,
     }
   }
 
-  fn compute(&mut self, input_signal: i32) -> Result<i32, &str> {
+  fn compute(&mut self) -> Result<i32, &str> {
     loop {
       let op = self.op_code[self.ptr] % 100;
       let res = (self.op_code[self.ptr] / 100) as i32;
@@ -112,22 +172,27 @@ impl IntCodeComputer {
           self.op_code[pos] = if op == 1 { v1 + v2 } else { v1 * v2 };
           self.ptr = self.ptr + 4;
         }
-        3 => {
-          let input = if self.is_phase_setting_used {
-            input_signal
-          } else {
-            self.is_phase_setting_used = true;
-            self.phase_setting
-          };
-          let pos = self.op_code[self.ptr + 1] as usize;
-          self.op_code[pos] = input;
-          self.ptr = self.ptr + 2;
-        }
+        3 => match self.receiver.recv() {
+          Ok(input) => {
+            let pos = self.op_code[self.ptr + 1] as usize;
+            self.op_code[pos] = input;
+            self.ptr = self.ptr + 2;
+          }
+          Err(_) => {
+            return Err("receiver error");
+          }
+        },
         4 => {
           let m = res % 10;
           let output = get_value(m, self.ptr + 1, &self.op_code);
           self.ptr = self.ptr + 2;
-          return Ok(output);
+          self.current_output = output;
+          match self.sender.send(output) {
+            Ok(_) => continue,
+            Err(_) => {
+              return Ok(self.current_output);
+            }
+          }
         }
         5 => {
           let m1 = res % 10;
@@ -170,7 +235,7 @@ impl IntCodeComputer {
           self.ptr = self.ptr + 4;
         }
         99 => {
-          return Err("Something when wrong with (99)");
+          return Ok(self.current_output);
         }
         _ => {
           return Err("Something when wrong");
